c65d264be71c32164e7114f5062916d87f0ea2d6
María Paz López <m.p.lopez.ponte@gmail.com>
2019-05-29 16:57:58 +0200

    Añadiendo la práctica final

---
+++ Practica final/ResumenAPP/app/src/main/java/com/example/resumenapp/MainActivity.java
@@ -0,0 +1,245 @@
+package com.example.resumenapp;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.SystemClock;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_asynctask;
+    private Button btn_receivers;
+    private Button btn_notificaciones;
+
+    private NotificationManager mNotificationManager;
+
+    private CustomReceiver mReceiver = new CustomReceiver();
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    // Constants for the notification actions buttons.
+    private static final String ACTION_UPDATE_NOTIFICATION =
+            "com.android.example.notifyme.ACTION_UPDATE_NOTIFICATION";
+    // Notification channel ID.
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+    // Notification ID.
+    private static final int NOTIFICATION_ID = 0;
+
+    private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
+
+    private NotificationManager mNotifyManager;
+    private ToggleButton alarmToggle;
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Name = "nameKey";
+    public static final String Phone = "phoneKey";
+    public static final String Email = "emailKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btn_asynctask = (Button) findViewById(R.id.btn_asynctask);
+        Button btn_receivers = (Button) findViewById(R.id.sendBroadcast);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        final EditText ed1=(EditText)findViewById(R.id.editText);
+        final EditText ed2=(EditText)findViewById(R.id.editText2);
+        final EditText ed3=(EditText)findViewById(R.id.editText3);
+
+        Button b1=(Button)findViewById(R.id.button);
+        final SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        mNotificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        btn_asynctask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String MY_URL = "http://www.ite.educacion.es/formacion/materiales/107/cd/html/pdf/html11.pdf";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(MY_URL)));
+            }
+        });
+
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String toastMessage;
+                if(isChecked){
+                    deliverNotification(MainActivity.this);
+                    //Set the toast message for the "on" case.
+                    toastMessage = "Stand Up Alarm On!";
+                } else {
+                    //Set the toast message for the "off" case.
+                    mNotificationManager.cancelAll();
+                    toastMessage = "Stand Up Alarm Off!";
+                }
+
+                //Show a toast to say the alarm is turned on or off.
+                Toast.makeText(MainActivity.this, toastMessage,Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n  = ed1.getText().toString();
+                String ph  = ed2.getText().toString();
+                String e  = ed3.getText().toString();
+
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                editor.putString(Name, n);
+                editor.putString(Phone, ph);
+                editor.putString(Email, e);
+                editor.commit();
+                Toast.makeText(MainActivity.this,"Thanks",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        createNotificationChannel();
+
+
+        SharedPreferences sharedPref =
+                android.support.v7.preference.PreferenceManager
+                        .getDefaultSharedPreferences(this);
+        Boolean switchPref = sharedPref.getBoolean
+                (SettingsActivity.KEY_PREF_EXAMPLE_SWITCH, false);
+        Toast.makeText(this, switchPref.toString(),
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendCustomBroadcast(View view) {
+
+        // Define the IntentFilter.
+        IntentFilter filter = new IntentFilter();
+        // Add system broadcast actions sent by the system when the power is
+        // connected and disconnected.
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver
+                (mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(customBroadcastIntent);
+
+    }
+
+    public void createNotificationChannel() {
+
+        // Create a notification manager object.
+        mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription
+                    ("Notifies every 15 minutes to stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context) {
+
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)
+                .setContentTitle("Stand Up Alert")
+                .setContentText("You should stand up and walk around now!")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+
+}
+
